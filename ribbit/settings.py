a78065b76cd3c8990f7acccf72b1136cf1e31bb6
Gaurav Narula <gnarula94@gmail.com>
2013-01-12 15:07:16 +0530

    Add ribbit_app

--- ribbit/settings.py
+++ ribbit/settings.py
@@ -121,6 +121,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'south',
+    'ribbit_app',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
