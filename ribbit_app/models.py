fd63418d48dd654d07955a0a198efa8dac325e63
Gaurav Narula <gnarula94@gmail.com>
2013-01-13 18:24:20 +0530

    Implemented User Profile and Following Relation

--- ribbit_app/models.py
+++ ribbit_app/models.py
@@ -6,3 +6,9 @@ class Ribbit(models.Model):
     content = models.CharField(max_length=140)
     user = models.ForeignKey(User)
     creation_date = models.DateTimeField(auto_now=True, blank=True)
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    follows = models.ManyToManyField('self', related_name='followed_by', symmetrical=False)
+
+User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
